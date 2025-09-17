b003e9c4ae849ef532a0864143b9c1cf74768cac
NgVietAh <ngvietanh10203@gmail.com>
2025-09-17 15:45:34 +0700

    update project add phan quyen, xac thuc, user endpoint

--- drf_tutorial/drftutorial/urls.py
+++ drf_tutorial/drftutorial/urls.py
@@ -10,6 +10,6 @@ from snippets import views
 urlpatterns = [
     # path('', include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('', include('snippets.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
