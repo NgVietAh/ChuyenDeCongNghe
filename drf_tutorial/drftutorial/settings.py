bfde03db98c67532fcbe6b2dad706989dbf5f100
NgVietAh <ngvietanh10203@gmail.com>
2025-09-17 15:07:50 +0700

    Thêm view SnippetList và SnippetDetail, cập nhật urls.py

--- drf_tutorial/drftutorial/settings.py
+++ drf_tutorial/drftutorial/settings.py
@@ -38,7 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'quickstart',
+    'snippets',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- drf_tutorial/drftutorial/urls.py
+++ drf_tutorial/drftutorial/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
-from rest_framework import routers
-from quickstart import views
+# from rest_framework import routers
+from snippets import views
+# from quickstart import views
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+# router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
 ]
